30acaec3c3a27e87125317af9366b6fc59d4f56c
Joy Thean <jct263@cornell.edu>
2021-05-05 15:53:03 -0400

    keyword bug fix

--- app/irsystem/controllers/search_controller.py
+++ app/irsystem/controllers/search_controller.py
@@ -1298,6 +1298,8 @@ def getPalettes(keywords, reqColors, energy):
     else:
         thresh = 105
 
+    sortedScored = []
+
     while len(sortedScored) < 5 and i < len(ranked):
         tup = ranked[i]
         tooClose = False
